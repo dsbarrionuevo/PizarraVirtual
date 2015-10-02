b55704f296a36df140caf12af6318d45c15fd2ac
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-10-02 16:32:45 -0300

    Organizo en paquetes la carpeta /js. Creo clases de la parte de chat pero están vacías.

---
+++ js/clases/dibujo/capa.class.js
@@ -0,0 +1,13 @@
+function Capa(orden) {
+    this.orden = orden;//algo asi como el z-index, o nivel de profundidad en el papel, 0 es la mas baja
+    this.objetos = [];//lista de objetos visualizables
+    this.dibujar = function (contexto) {
+        for (var i = 0; i < this.objetos.length; i++) {
+            this.objetos[i].dibujar(contexto);
+        }
+    };
+    this.agregarObjeto = function (objeto) {
+        //objeto es instancia de Visualizable
+        this.objetos.push(objeto);//por ahora no me procupo por el orden de visualizacion dentro de la capa
+    };
+}
