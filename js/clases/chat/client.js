a314a875759a473d2df0e0a11ed6c9cb26a22b9b
Fede <fede@federiver@gmail.com>
2015-10-06 16:47:52 -0300

    Agrego la parte del chat

---
+++ js/clases/chat/client.js
@@ -0,0 +1,73 @@
+var ws = io.connect('http://localhost:8000'),
+lastPress = null,
+nick = '';
+
+$(document).on('ready', function escuchar(){
+
+
+	ws.on('mensajeServer', function recibir(datosServer){
+		$('#conversacion').append($(' <p>').text(datosServer.nick +": " +datosServer.mensaje));	
+	});
+
+
+
+	$('#enviar').on('click', enviarMensaje);
+
+	$('#mensaje').on('focus', function (){
+		$('#mensaje').on('keypress',function (evt){
+			if(evt.which === 13) 
+			{
+				enviarMensaje();
+				evt.preventDefault();
+			}
+		});
+	});
+
+	function enviarMensaje()
+	{
+		ws.emit('mensajeUser',{mensaje:$('#mensaje').val(), de:nick});
+		$('#mensaje').val('');
+		$('#escribiendo').val('');
+
+	}
+
+	ws.on('emitirNicks', function imprimir(nicks){
+			var cadena = '';
+		for (var i = 0; i < nicks.length; i++) {
+			cadena += "| " + nicks[i].nick + ' |';
+			console.log(cadena);
+		}
+		$('#nicks').text(cadena);
+	});
+
+	$('#enviarNick').on('click', function (){
+		if ($('#nick').val() != '')
+		ws.emit('addNickServer',{nick:$('#nick').val()});
+	});
+
+	ws.on('nickAceptado', function(user){
+		nick = user; //Corregir cuando se mande el evento solo al cliente que corresponde
+		$("#enviarNick").prop('disabled',true);
+	});
+
+	$('#mensaje').on('keydown', function() {
+			ws.emit('escribiendo');
+			});
+		
+		ws.on('imprimirEscribiendo', function(){
+			$('#escribiendo').val('escribiendo...');
+
+
+		ws.on('borrarEscribiendo',
+			function (){
+			$('#escribiendo').val('');
+			});
+		});
+
+	$('#desconectar').on('click', function(){
+		ws.emit('desconectar', nick);
+		$('#nick').val('');
+		//$('#')		
+	});
+
+});
