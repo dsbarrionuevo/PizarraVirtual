23028d653992b7ede6a9d0f42c9c4f70e24ff31e
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-10-06 17:58:27 -0300

    Planteo la logia de desmarcar.

--- js/clases/dibujo/herramienta_seleccion.class.js
+++ js/clases/dibujo/herramienta_seleccion.class.js
@@ -1,6 +1,13 @@
 HerramientaSeleccion.prototype = new Herramienta;
+/**
+ * Permite seleccionar solo un objeto a la vez
+ * @param {string} nombre Nombre de la herramienta
+ * @param {Papel} papel Papal de dibujo
+ * @returns {HerramientaSeleccion}
+ */
 function HerramientaSeleccion(nombre, papel) {
     Herramienta.call(this, nombre, papel);
+    this.ultimoSeleccionado = null;
     this.onclick = function (evento) {
         this.__proto__.onclick(evento);
         var x = evento.offsetX;
@@ -8,7 +15,17 @@ function HerramientaSeleccion(nombre, papel) {
         var objetos = this.papel.obtenerObjetosEnPunto(x, y);
         if (objetos.length > 0) {
             var ultimoVisible = objetos[objetos.length - 1];
+            if (this.ultimoSeleccionado !== null) {
+                //this.ultimoSeleccionado.desmarcar();
+            }
             ultimoVisible.marcar(this.papel.contexto);
+            this.ultimoSeleccionado = ultimoVisible;
+        } else {
+            //no hay nada donde hice click...
+            if (this.ultimoSeleccionado !== null) {
+                //this.ultimoSeleccionado.desmarcar();
+            }
+            this.ultimoSeleccionado = null;
         }
     };
 }
