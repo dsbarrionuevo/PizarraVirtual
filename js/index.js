e3661bc42e5e7abcaf210df25e9f2bb8a5d6f7ae
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-10-06 15:08:32 -0300

    Quito comentarios de index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,6 @@
 var papel;
 window.addEventListener("load", function () {
     papel = new Papel(document.querySelector("canvas"));
-//    var r1 = new Rectangulo(10, 10, 20, 20);
-//    var r2 = new Rectangulo(40, 10, 20, 20);
-//    var c1 = new Circulo(80, 20, 10, new Estilo(function (contexto) {
-//        contexto.save();
-//        contexto.fillStyle = "#f00";
-//    }));
-//    var c2 = new Circulo(papel.canvas.width / 2, papel.canvas.height / 2, 100);
-//    
 
     var c1 = new Triangulo(50, 50, 100,100, new Estilo(function (contexto) {
         contexto.save();
